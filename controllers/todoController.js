b8631430f98d5fd8acec5453c70a00f030c44db3
Daksh Miglani <dakshmiglani@yandex.com>
2016-12-10 22:19:01 +0530

    Used Mongoose to integrate Mongodb and Completed the Product.
    Finished the Application.

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,13 +1,42 @@
+var bodyParser = require('body-parser');
+// MongoDB
+var mongoose = require('mongoose');
+// connecting MongoDB
+mongoose.connect('mongodb://daksh:test123@ds059712.mlab.com:59712/todoappdm');
+
+// create a schema - like a blueprint for mongoDB
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+// Model of the MVC.
+var Todo = mongoose.model('Todo', todoSchema);
+// basic data/dummy data
+// var data = [{item: 'get milk'}, {item: 'make an app'}, {item: 'watch a movie'}];
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 // exporting the controllers to the app.js
 module.exports = function(app){
   // http verbs for handling requests and responses.
   app.get('/todo', function(req, res){
-    res.render('todo');
+    // passing data. get data from mongo and pass through view
+    Todo.find({}, function(err, data){
+      if(err) throw err;
+      res.render('todo', {todos: data});
+    });
   });
-  app.post('/todo', function(req, res){
-
-  });
-  app.delete('/todo', function(req, res){
-
+  // middle ware added.
+  // Don't forget the urlencodedParser, makes problems :P
+  // get data and post to mongo
+  app.post('/todo', urlencodedParser, function(req, res){
+    var newTodo = Todo(req.body).save(function(err, data){
+      if(err) throw err;
+      res.json(data);
+    })
+    });
+    // delete item from mongoNOW
+  app.delete('/todo/:item',function(req, res){
+    Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data){
+      if (err) throw err;
+      res.json(data);
+    });
   });
 };
